2dc6b4c6b9d9d4c9869bbb77961ffe57d0738989
mazay1986 <60013249+mazay1986@users.noreply.github.com>
2021-03-12 20:22:29 +0200

    yuotube libarry+

---
+++ app/src/main/java/com/team23/androidacademyapp/ui/MentorsViewModel.kt
@@ -0,0 +1,7 @@
+package com.team23.androidacademyapp.ui
+
+import androidx.lifecycle.ViewModel
+
+class MentorsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/team23/androidacademyapp/ui/NewsViewModel.kt
@@ -0,0 +1,7 @@
+package com.team23.androidacademyapp.ui
+
+import androidx.lifecycle.ViewModel
+
+class NewsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
