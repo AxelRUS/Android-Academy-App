374a052fd79caee17c9a375f1ab9de02ca587a0e
pavelegorenkov <epipasha@gmail.com>
2021-03-12 22:38:34 +0300

    Add mentor adapter

--- app/src/main/java/com/team23/androidacademyapp/ui/MentorsViewModel.kt
+++ app/src/main/java/com/team23/androidacademyapp/ui/MentorsViewModel.kt
@@ -1,7 +1,17 @@
 package com.team23.androidacademyapp.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MentorsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _mutableMentorsList = MutableLiveData<List<Mentor>>(emptyList())
+
+    val mentorsList: LiveData<List<Mentor>> get() = _mutableMentorsList
+
+    init {
+        _mutableMentorsList.value = listOf(Mentor(name = "Test"))
+    }
+
 }
