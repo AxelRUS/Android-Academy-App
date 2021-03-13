20f401af7eed9ab520a93e7cdeab1aba2e64269f
mazay1986 <60013249+mazay1986@users.noreply.github.com>
2021-03-13 08:43:57 +0200

    ++

--- app/src/main/java/com/team23/androidacademyapp/domain/LectureViewHolder.kt
+++ app/src/main/java/com/team23/androidacademyapp/domain/LectureViewHolder.kt
@@ -1,6 +1,7 @@
 package com.team23.androidacademyapp.domain
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 import com.team23.androidacademyapp.R
@@ -8,8 +9,12 @@ import com.team23.androidacademyapp.R
 class LectureViewHolder (itemView: View):RecyclerView.ViewHolder(itemView){
 
     private val youTubePlayerView:YouTubePlayerView=itemView.findViewById(R.id.youtube_pleer)
+    private val tvTitle:TextView = itemView.findViewById(R.id.tv_title)
+    private val tvDescription:TextView = itemView.findViewById(R.id.tv_description)
 
     fun bind(model: Model){
+        tvTitle.text=model.title
+        tvDescription.text=model.description
        // youTubePlayerView.loadVideo(model.video)
     }
 
---
+++ app/src/main/java/com/team23/androidacademyapp/domain/ModelMentors.kt
@@ -0,0 +1,4 @@
+package com.team23.androidacademyapp.domain
+
+ data class ModelMentors(val name:String,val surname:String,val contact:String,) {
+}
