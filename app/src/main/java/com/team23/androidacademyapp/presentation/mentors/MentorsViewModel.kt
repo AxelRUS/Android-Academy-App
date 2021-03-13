89ce279cbdfec26194327d91a9a89bf6abffe916
mazay1986 <60013249+mazay1986@users.noreply.github.com>
2021-03-13 10:11:28 +0200

    ++1

--- app/src/main/java/com/team23/androidacademyapp/presentation/mentors/MentorsViewModel.kt
+++ app/src/main/java/com/team23/androidacademyapp/presentation/mentors/MentorsViewModel.kt
@@ -1,7 +1,34 @@
 package com.team23.androidacademyapp.presentation.mentors
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.FirebaseFirestore
+import com.team23.androidacademyapp.domain.Model
 
 class MentorsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val firebaseFirestone: FirebaseFirestore = FirebaseFirestore.getInstance()
+    val modelMentor: MutableLiveData<MutableList<Model>> = MutableLiveData()
+    val modelLiveData: LiveData<MutableList<Model>>
+        get() = modelMentor
+
+    init {
+        FirebaseFirestore.getInstance().collection("lecture").get()
+            .addOnSuccessListener { result ->
+                val listData = mutableListOf<Model>()
+                for (document in result) {
+                    val mVideo: String = document.getString("video") ?: ""
+                    val mTitle: String = document.getString("title") ?: ""
+                    val mDescription: String = document.getString("description") ?: ""
+                    Log.e("XXX",mVideo)
+
+                    val model = Model(
+                        mVideo,mTitle,mDescription)
+                    listData.add(model)
+                }
+                modelMentor.value = listData
+            }
+    }
 }
---
+++ app/src/main/java/com/team23/androidacademyapp/presentation/mentors/ModelMentor.kt
@@ -0,0 +1,4 @@
+package com.team23.androidacademyapp.presentation.mentors
+
+data class ModelMentor (val name:String,val surname:String,val contact:String,val foto:String, ) {
+}
